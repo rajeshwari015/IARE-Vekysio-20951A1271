b480120235c1884d4a78cf866e475fd7db3021bb
Pavuluri Rajeshwari <81892753+rajeshwari015@users.noreply.github.com>
2023-03-02 20:25:25 +0530

    Update strrev.java

--- strrev.java
+++ strrev.java
@@ -1,16 +1,18 @@
-import java.io.*;
-import java.util.io;
-
-public class strrev{
-    public static void main(String args[]){
-    string s=sc.nextline();
-
-    StringBuilder reverseString = new StringBuilder(s);
-       reverseString.reverse();  
-       String result = reverseString.toString();
-       System.out.println("Reversed string: "+result); 
-
-
-   }
-
-} 
+import java.util.Scanner;
+class ReverseofaString
+{
+	String reverse(String s)
+	{
+	 if(s.length() == 0)
+     	 return " ";
+  	 return s.charAt(s.length()-1) + reverse(s.substring(0,s.length()-1));
+	}
+	public static void main(String[ ] arg)
+	{
+        ReverseofaString rev=new ReverseofaString();
+        Scanner sc=new Scanner(System.in);
+	    System.out.print("Enter a string : ");
+	    String  str=sc.nextLine();	
+	    System.out.println("Reverse of a String  :"+rev.reverse(str));
+	}	
+}
